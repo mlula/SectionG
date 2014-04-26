9c7854a0a81ad1e1ba34f71afa936086f9a610d6
Sébastien Richer <sebastienricher@gmail.com>
2014-04-26 15:41:37 -0400

    Form version 0.1

--- Website/SectionG/Scripts/main.js
+++ Website/SectionG/Scripts/main.js
@@ -2,8 +2,15 @@
     console.log("submitLease!");
     ajaxPost("/Umbraco/Surface/Main/SubmitLease/", "", function (result) {
         console.log("submitLease: " + result);
+        $('#step3').hide();
+        $('#step4').show();
     })
     return false;
+}
+
+function goToRegistry() {
+    // ...
+    console.log("goToRegistry!");
 }
 
 function ajaxPost(url, data, callBack) {
