0716611ffc83a4de905444d09d2b66b96861dd87
mlula <mario_lula@hotmail.com>
2014-04-26 16:19:11 -0400

    Nouveaux champs dans la BD

---
+++ Website/SectionG/Code/Pocos/Borough.cs
@@ -0,0 +1,16 @@
+﻿using PetaPoco;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SectionG.Code.Pocos
+{
+    [PetaPoco.TableName("_sg_Borough")]
+    [PetaPoco.PrimaryKey("Id")]
+    public class Borough
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
